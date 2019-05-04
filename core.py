ea91a3d8f5345b5bb18bf59e395ca2471db8fef3
Plover-Y <a041011p@yahoo.co.jp>
2019-05-04 20:50:18 +0900

    test

--- core.py
+++ core.py
@@ -16,9 +16,9 @@ def login(wiki, username, password):
         print('no valid session file found')
     session = requests.Session()
     payload = {'action': 'login', 'lgname': username, 'lgpassword': password, 'format': 'json'}
-    r = session.post('https://community.wikia.com/api.php', data=payload, headers=headers)
+    r = session.post('https://community.fandom.com/api.php', data=payload, headers=headers)
     payload['lgtoken'] = r.json()['login']['token']
-    r = session.post('https://community.wikia.com/api.php', data=payload, headers=headers)
+    r = session.post('https://community.fandom.com/api.php', data=payload, headers=headers)
     # print(r.json()['login']['result'])
     # TODO: test for login failures
     pickle.dump(session, open(os.environ['HOME']+'/.discussions-bot/session.p', 'wb'))
@@ -27,15 +27,15 @@ def login(wiki, username, password):
 # bool: true if logged in as provided user, false for other user or anon
 def is_logged_in(session, username, wiki):
     payload = {'action': 'query', 'meta': 'userinfo', 'format': 'json'}
-    r = session.get('https://community.wikia.com/api.php', params=payload, headers=headers)
+    r = session.get('https://community.fandom.com/api.php', params=payload, headers=headers)
     return r.json()['query']['userinfo']['name'] == username
 
 def get_edit_token(session, wiki):
     payload = {'action': 'query', 'prop': 'info', 'intoken': 'edit', 'titles': '#', 'format': 'json'}
-    r = session.post('https://community.wikia.com/api.php', data=payload, headers=headers)
+    r = session.post('https://community.fandom.com/api.php', data=payload, headers=headers)
     return r.json()['query']['pages']['-1']['edittoken']
 
 def get_wiki_id(session, wiki):
     payload = {'action': 'query', 'meta': 'siteinfo', 'siprop': 'wikidesc', 'format': 'json'}
-    r = session.get('https://'+wiki+'.wikia.com/api.php', params=payload, headers=headers)
+    r = session.get('https://'+wiki+'/api.php', params=payload, headers=headers)
     return r.json()['query']['wikidesc']['id']
